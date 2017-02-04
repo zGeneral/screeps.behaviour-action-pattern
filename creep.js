ac025b227f995d30b19e51b4800612935857a1bc
Karl the Pagan <karlthepagan@gmail.com>
2017-02-04 14:22:34 -0800

    new strategy for avoiding bad rooms

--- creep.js
+++ creep.js
@@ -218,58 +218,41 @@ mod.extend = function(){
         let range = undefined;
         let tempTarget = targetPos;
         let maxRooms = 16; // screeps default
+        let path;
         if (ROUTE_PRECALCULATION && this.pos.roomName != targetPos.roomName) {
             var route = this.room.findRoute(targetPos.roomName);
             if ( route.length > 1 ) {
                 targetPos = new RoomPosition(25, 25, route[1].room);
-                this.roomPositionBias(targetPos);
-                range = Math.min(24 - Math.abs(targetPos.x - 25), 24 - Math.abs(targetPos.y - 25));
-                ignoreCreeps = true;
-                if( DEBUG && TRACE ) trace('Creep', {creepName:this.name, targetPos, range, getPath:'routePrecalc', Creep:'getPath'});
+                range = 24;
+                maxRooms = 2;
             } else {
                 maxRooms = 1;
             }
+            const roomsOnPath = _.groupBy(route, 'room');
+            path = this.room.findPath(this.pos, targetPos, {
+                serialize: true,
+                ignoreCreeps,
+                range,
+                maxRooms,
+                costCallback: function (roomName, cost) {
+                    return roomsOnPath[roomName] ? cost : mod.blockedRoom;
+                }
+            });
+            if( DEBUG && TRACE ) trace('Creep', {creepName:this.name, targetPos, range, path:path&&path.length, getPath:'route precalc', Creep:'getPath'});
+        } else {
+            path = this.room.findPath(this.pos, targetPos, {
+                serialize: true,
+                ignoreCreeps,
+                range,
+                maxRooms
+            });
+            if( DEBUG && TRACE ) trace('Creep', {creepName:this.name, targetPos, range, path:path&&path.length, getPath:'path', Creep:'getPath'});
         }
 
-        let path = this.room.findPath(this.pos, targetPos, {
-            serialize: true,
-            ignoreCreeps: ignoreCreeps,
-            range,
-            maxRooms: maxRooms
-        });
         if( path && path.length > 4 )
             return path.substr(4);
         else return null;
     };
-    Creep.prototype.roomPositionBias = function(targetPos) {
-        const startRoom = this.pos.roomName;
-        const exits = Game.map.describeExits(targetPos.roomName);
-        // move target away from exits based on their weight
-        for( const dir in exits ) {
-            switch(+dir) {
-                case TOP:
-                    targetPos.y = targetPos.y +
-                        (routeRange(startRoom, exits[dir]) + (ROUTE_ROOM_COST[exits[dir]] || 1) - 2);
-                    break;
-                case BOTTOM:
-                    targetPos.y = targetPos.y -
-                        (routeRange(startRoom, exits[dir]) + (ROUTE_ROOM_COST[exits[dir]] || 1) - 2);
-                    break;
-                case LEFT:
-                    targetPos.x = targetPos.x +
-                        (routeRange(startRoom, exits[dir]) + (ROUTE_ROOM_COST[exits[dir]] || 1) - 2);
-                    break;
-                case RIGHT:
-                    targetPos.x = targetPos.x -
-                        (routeRange(startRoom, exits[dir]) + (ROUTE_ROOM_COST[exits[dir]] || 1) - 2);
-                    break;
-            }
-        }
-        if( targetPos.y !== 25) targetPos.y = targetPos.y > 25 ? 35 : 15;
-        if( targetPos.x !== 25) targetPos.x = targetPos.x > 25 ? 35 : 15;
-        return targetPos;
-    };
-
     Creep.prototype.fleeMove = function() {
         if( DEBUG && TRACE ) trace('Creep', {creepName:this.name, Action:'fleeMove', Creep:'run'});
         let drop = r => { if(this.carry[r] > 0 ) this.drop(r); };
@@ -528,3 +511,5 @@ mod.register = function() {
         if (Creep.setup[setup].register) Creep.setup[setup].register(this);
     }
 };
+const ffBlock = _.fill(new Array(616), 4294967295);
+mod.blockedRoom = PathFinder.CostMatrix.deserialize(ffBlock);
